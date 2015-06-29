c0ed2e25d3b362d1c3d41a7a9375ad7990cdfb45
AradoBlitz <schlachtnavaho@gmail.com>
2015-06-29 14:40:58 +0300

    Incapsulated reading first example properties in to PropertyTokinizer.readFirstProperties method.

--- src/satellite/device/image/xml/PropertyTokenizer.java
+++ src/satellite/device/image/xml/PropertyTokenizer.java
@@ -5,5 +5,18 @@ public class PropertyTokenizer {
 	public String name;
 	public char type;
 	public String value;
+	public void readFirstProperty(String input) {
+		StringBuffer buf = new StringBuffer();
+		int i = 0;
+		type = input.charAt(i);
+		for(i++;'='!=input.charAt(i);i++)
+			buf.append(input.charAt(i)); 
+		name = buf.toString();
+		
+		buf = new StringBuffer();		
+		for(i++;'n'!=input.charAt(i);i++)
+			buf.append(input.charAt(i));
+		value = buf.toString();
+	}
 
 }
--- test/satellite/device/image/xml/PropertyReaderTest.java
+++ test/satellite/device/image/xml/PropertyReaderTest.java
@@ -12,21 +12,12 @@ public class PropertyReaderTest {
 	@Test
 	public void parsePropertiesInSingleLine() throws Exception {
 		
-		StringBuffer buf = new StringBuffer();
+		
 		List<String> result = new ArrayList<String>();
 		String input = "nStringWidth=1632nMiddlePixel=820";
 		PropertyTokenizer tokenizer = new PropertyTokenizer();
-		int i = 0;
-		tokenizer.type = input.charAt(i);
-		for(i++;'='!=input.charAt(i);i++)
-			buf.append(input.charAt(i)); 
-		tokenizer.name = buf.toString();
 		
-		buf = new StringBuffer();		
-		for(i++;'n'!=input.charAt(i);i++)
-			buf.append(input.charAt(i));
-		tokenizer.value = buf.toString();
-		
+		tokenizer.readFirstProperty(input);
 		assertEquals('n', tokenizer.type);
 		assertEquals("StringWidth", tokenizer.name);
 		assertEquals("1632",tokenizer.value);
