620896ab3286e950350d7f94f078dfbf891de877
AradoBlitz <schlachtnavaho@gmail.com>
2015-06-30 14:10:09 +0300

    Removed readFirstPropertyMethod.

--- src/satellite/device/image/xml/PropertyTokenizer.java
+++ src/satellite/device/image/xml/PropertyTokenizer.java
@@ -7,20 +7,6 @@ public class PropertyTokenizer {
 	public String value;
 	private int index;
 	
-	public void readFirstProperty(String input) {
-		StringBuffer buf = new StringBuffer();
-;
-		type = input.charAt(index);
-		for(index++;'='!=input.charAt(index);index++)
-			buf.append(input.charAt(index)); 
-		name = buf.toString();
-		
-		buf = new StringBuffer();		
-		for(index++;index<input.length()&&'n'!=input.charAt(index);index++)
-			buf.append(input.charAt(index));
-		value = buf.toString();
-	}
-	
 	public void readLastProperty(String input) {
 
 		type = input.charAt(index);
