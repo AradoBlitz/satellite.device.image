fdcbee92f718ed9b220f1daf9a17281c472b6a5a
AradoBlitz <schlachtnavaho@gmail.com>
2015-06-30 14:42:15 +0300

    Moved input parameter from readNextProperty method to constructor.

--- src/satellite/device/image/xml/PropertyTokenizer.java
+++ src/satellite/device/image/xml/PropertyTokenizer.java
@@ -6,8 +6,13 @@ public class PropertyTokenizer {
 	public char type;
 	public String value;
 	private int index;
+	private String input;
 	
-	public void readNextProperty(String input) {
+	public PropertyTokenizer(String input) {
+		this.input = input;
+	}
+
+	public void readNextProperty() {
 
 		type = input.charAt(index);
 		StringBuffer buf = new StringBuffer();
--- test/satellite/device/image/xml/PropertyReaderTest.java
+++ test/satellite/device/image/xml/PropertyReaderTest.java
@@ -7,14 +7,14 @@ import org.junit.Test;
 public class PropertyReaderTest {
 	
 	String input = "nStringWidth=1632nMiddlePixel=820";
-	PropertyTokenizer tokenizer = new PropertyTokenizer();
+	PropertyTokenizer tokenizer = new PropertyTokenizer(input);
 	
 	@Test
 	public void parseFirstProperty() throws Exception {
 			
 		
 		
-		tokenizer.readNextProperty(input);
+		tokenizer.readNextProperty();
 		assertEquals('n', tokenizer.type);
 		assertEquals("StringWidth", tokenizer.name);
 		assertEquals("1632",tokenizer.value);
@@ -23,8 +23,8 @@ public class PropertyReaderTest {
 	@Test
 	public void parseLastProperty() throws Exception {
 	
-		tokenizer.readNextProperty(input);
-		tokenizer.readNextProperty(input);
+		tokenizer.readNextProperty();
+		tokenizer.readNextProperty();
 		assertEquals('n', tokenizer.type);
 		assertEquals("MiddlePixel", tokenizer.name);
 		assertEquals("820", tokenizer.value);
